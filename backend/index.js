865f679bbfe41816f0806bfbfd3ee2a6f68fe8bc
Aryan Yadav <caryan830@gmail.com>
2026-05-31 19:14:54 +0530

    add backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/login',(req,res)=>{
+    res.send('this is login p0age')
+})
+app.get('/logout',(req,res)=>{
+    res.send('thsis is logout page')
+})
+
+app.get('/logout',(req,res)=>{
+    res.send('thsis is logout page')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
